2d082415f9e8f0fa332e56d383cf4b555f01a350
凡超快点跑 <17610388360@163.com>
2022-01-20 14:27:16 +0800

    - fix text

--- src/js/video.js
+++ src/js/video.js
@@ -19,7 +19,7 @@ function onYouTubeIframeAPIReady() {
     player = new window.YT.Player('videoplayer', {
       height: '274',
       width: '480',
-      videoId: 'KXdoyiKVAgU',
+      videoId: '08-5p_9ExvA',
       events: {
         'onReady': onPlayerReady,
         // 'onStateChange': onPlayerStateChange
