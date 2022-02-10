d63855374f6c60b5b136ce39010f502a369532da
凡超快点跑 <17610388360@163.com>
2022-02-10 10:29:41 +0800

    - fix video async

--- src/js/video.js
+++ src/js/video.js
@@ -2,14 +2,38 @@
 let player;
 
 $(document).ready(function () {
-  onYouTubeIframeAPIReady()
+
+  loadScript(function () {
+    onYouTubeIframeAPIReady()
+  })
+
 })
 
+function loadScript(callback) {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  if (script.readyState) {
+    script.onreadystatechange = function () {
+      if (script.readyState == "complete" || script.readyState == "loaded") {
+        // 回调加载的js方法  加载完成
+        callback();
+      }
+    }
+  } else {
+    script.onload = function () {
+      // 回调加载的js方法  加载完成
+      callback();
+    }
+
+  }
+  script.src = 'https://www.youtube.com/iframe_api'; //确保事件绑定完毕之后再去加载触发事件回调
+  document.head.appendChild(script);
+}
 
 $(".scroll").click(function (event) {
   let target = event.currentTarget.innerHTML
 
-  if(target === 'M1') player.playVideo()
+  if (target === 'M1') player.playVideo()
 })
 
 
@@ -29,7 +53,7 @@ function onYouTubeIframeAPIReady() {
 }
 
 function onPlayerReady(event) {
-//   event.target.playVideo()
+  //   event.target.playVideo()
 }
 
 let done = false;
